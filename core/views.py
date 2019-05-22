87ce867413fcc5979ef353a4b5cda3d9d86bc7f0
jedrzejmatuszak <jedrzej.matuszak@gmail.com>
2019-05-22 10:03:08 +0200

    signup mod

--- core/views.py
+++ core/views.py
@@ -11,10 +11,14 @@ class LandingPage(View):
     def get(self, request):
         return render(request, 'index.html')
 
+    def post(self, request):
+
+        return HttpResponse()
+
 
 class SingUpView(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('landing_page')
     template_name = 'register.html'
 
 
